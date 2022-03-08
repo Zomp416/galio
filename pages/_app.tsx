40f73eeaf48d22905083f3c7ff85ec919fa4dd79
clucidojr123 <clucidojr123@gmail.com>
2022-03-08 12:31:48 -0500

    add mui theme and overpass font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: ["Overpass", "sans-serif"].join(","),
+    },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
