6820834593cc33ff7c847531fa956161654db3d5
Jack <jack.liu.1@stonybrook.edu>
2022-04-06 18:13:37 -0400

    Split Home Page into Post-Login and Landing Page depending on the user login status

---
+++ components/landing/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Navbar from "../navbar";
+
+const LandingPage: NextPage = () => {
+    return (
+        <div>
+            <Navbar props="comics" />
+            landing!!
+        </div>
+    );
+};
+
+export default LandingPage;
---
+++ components/postlogin/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Navbar from "../navbar";
+
+const PostLoginPage: NextPage = () => {
+    return (
+        <div>
+            <Navbar props="comics" />
+            postlogin
+        </div>
+    );
+};
+
+export default PostLoginPage;
