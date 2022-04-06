2d10a4e728e3daf54336430213ab04d0a03274cb
keith <keithohno@gmail.com>
2022-04-06 13:00:58 -0700

    added comic grid

---
+++ components/comic/cards/simple/styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const Card = styled.div`
+    width: 100px;
+    height: 100px;
+    margin: 10px;
+    background-color: gray;
+    display: flex;
+`;
