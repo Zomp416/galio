f0b011417ea61754f4ca3c234ea64fb3e9cbac0d
clucidojr123 <clucidojr123@gmail.com>
2022-04-07 23:07:00 -0400

    update edit story to use quill:

--- components/story/edit/index.tsx
+++ components/story/edit/index.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import {
+    Box,
+    Button,
+    Typography,
+    IconButton,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Dialog,
+    DialogTitle,
+    TextField,
+} from "@mui/material";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import FormatBoldIcon from "@mui/icons-material/FormatBold";
-import FormatItalicIcon from "@mui/icons-material/FormatItalic";
-import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-import FormatColorTextIcon from "@mui/icons-material/FormatColorText";
-import BrushIcon from "@mui/icons-material/Brush";
-import ImageIcon from "@mui/icons-material/Image";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
-import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
-import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
-import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
 import * as Styled from "./styles";
+import "react-quill/dist/quill.snow.css";
+import dynamic from "next/dynamic";
+
+const ReactQuill = dynamic(import("react-quill"), {
+    ssr: false,
+    loading: () => <div>Loading Text Editor...</div>,
+});
 
 const story = {
     _id: "a2",
@@ -32,8 +43,72 @@ const story = {
 };
 
 const ViewStory: React.FC = () => {
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [changed, setChanged] = useState<boolean>(false);
+    const [tags, setTags] = useState<string[]>(["Comedy", "College"]);
+    const [addTag, setAddTag] = useState<string>("");
+
     return (
         <>
+            <Dialog
+                open={modalOpen}
+                onClose={() => {
+                    setModalOpen(false);
+                }}
+                fullWidth
+                PaperProps={{
+                    style: {
+                        backgroundColor: "#E6F4F1",
+                    },
+                }}
+            >
+                <DialogTitle sx={{ padding: "16px 16px" }}>Manage Tags</DialogTitle>
+                <List>
+                    {tags.map((val, index) => (
+                        <ListItem key={`${index}-modal-tag`}>
+                            <ListItemText primary={val} />
+                            <ListItemAvatar>
+                                <IconButton
+                                    onClick={() => {
+                                        setTags(tags.filter(tag => tag !== val));
+                                    }}
+                                >
+                                    <DeleteIcon />
+                                </IconButton>
+                            </ListItemAvatar>
+                        </ListItem>
+                    ))}
+                    <ListItem>
+                        <ListItemText
+                            primary={
+                                <TextField
+                                    margin="dense"
+                                    id="addTag"
+                                    label="Add Tag"
+                                    type="text"
+                                    fullWidth
+                                    variant="standard"
+                                    value={addTag}
+                                    onChange={e => {
+                                        setAddTag(e.target.value);
+                                    }}
+                                />
+                            }
+                        />
+                        <ListItemAvatar>
+                            <IconButton
+                                disabled={!addTag}
+                                onClick={() => {
+                                    setTags([...tags, addTag]);
+                                    setAddTag("");
+                                }}
+                            >
+                                <AddIcon />
+                            </IconButton>
+                        </ListItemAvatar>
+                    </ListItem>
+                </List>
+            </Dialog>
             <Styled.ViewStoryContainer>
                 <Styled.RowContainer>
                     <Box
@@ -47,125 +122,64 @@ const ViewStory: React.FC = () => {
                         src={story.splashURL}
                     />
                     <Styled.ColumnContainer>
-                        <Typography variant="h4" width={"100%"} sx={{ paddingTop: "10px" }}>
+                        <Typography
+                            variant="h4"
+                            width={"100%"}
+                            sx={{ paddingTop: "10px", marginBottom: "5px" }}
+                        >
                             Story Title
+                            <IconButton>
+                                <EditIcon color="primary" />
+                            </IconButton>
                         </Typography>
                         <Styled.TVContainer>
                             <Styled.TagsContainer>
-                                <Styled.Tag
-                                    variant="contained"
-                                    color="secondary"
-                                    size="small"
-                                    sx={{ textTransform: "none" }}
+                                {tags.map((val, index) => (
+                                    <Styled.Tag key={`${index}-tag`}>{val}</Styled.Tag>
+                                ))}
+                                <Styled.ManageTag
+                                    onClick={() => {
+                                        setModalOpen(true);
+                                    }}
                                 >
-                                    Comedy
-                                </Styled.Tag>
-                                <Styled.Tag
-                                    variant="contained"
-                                    color="secondary"
-                                    size="small"
-                                    sx={{ textTransform: "none" }}
-                                >
-                                    College
-                                </Styled.Tag>
+                                    Manage Tags
+                                </Styled.ManageTag>
                             </Styled.TagsContainer>
                         </Styled.TVContainer>
                     </Styled.ColumnContainer>
                 </Styled.RowContainer>
                 <Styled.Story>
-                    <AppBar position="static" color="primary" sx={{ height: "50px" }}>
-                        <Toolbar
-                            disableGutters
-                            style={{
-                                minHeight: "50px",
-                            }}
-                            sx={{
-                                display: "flex",
-                                justifyContent: "space-evenly",
-                                alignItems: "center",
+                    <Styled.ButtonsContainer>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            startIcon={<SaveIcon />}
+                            onClick={() => {
+                                setChanged(false);
                             }}
                         >
-                            {/* TODO: IMPLEMENT BUTTON OVER THE ICONS */}
-                            <Box>
-                                <FormatBoldIcon />
-                                <FormatItalicIcon
-                                    sx={{
-                                        marginLeft: "10px",
-                                        marginRight: "10px",
-                                    }}
-                                />
-                                <FormatUnderlinedIcon />
-                            </Box>
-                            <FormatColorTextIcon />
-                            <BrushIcon />
-                            <ImageIcon />
-                            <FormatListBulletedIcon />
-                            <Box>
-                                <FormatAlignLeftIcon />
-                                <FormatAlignCenterIcon
-                                    sx={{
-                                        marginLeft: "10px",
-                                        marginRight: "10px",
-                                    }}
-                                />
-                                <FormatAlignRightIcon />
-                            </Box>
-                            <DeleteOutlineIcon />
-                        </Toolbar>
-                    </AppBar>
-                    <Styled.ButtonsContainer>
-                        <Button variant="contained" color="primary">
-                            <ChevronLeftIcon />
-                            Title
+                            Save {changed && "*"}
                         </Button>
                         <Typography variant="h4" sx={{ fontWeight: "bold" }}>
                             Chapter 1
+                            <IconButton>
+                                <EditIcon color="primary" />
+                            </IconButton>
                         </Typography>
-                        <Button variant="contained" color="primary">
+                        <Button variant="contained" color="primary" endIcon={<ChevronRightIcon />}>
                             Chapter 2
-                            <ChevronRightIcon />
                         </Button>
                     </Styled.ButtonsContainer>
-                    <Box
-                        border={1}
-                        borderColor="#3390FF"
-                        sx={{
-                            padding: "10px",
+                    <ReactQuill
+                        defaultValue={story.description}
+                        placeholder="Start Typing..."
+                        style={{ flex: 1 }}
+                        onChange={(_, __, source) => {
+                            if (!changed && source === "user") {
+                                setChanged(true);
+                            }
                         }}
-                    >
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Amet consectetur
-                            adipiscing elit ut aliquam purus sit. Ipsum dolor sit amet consectetur
-                            adipiscing. Vulputate enim nulla aliquet porttitor lacus luctus. Risus
-                            nullam eget felis eget. Volutpat consequat mauris nunc congue nisi.
-                            Vitae tortor condimentum lacinia quis vel eros donec. Mauris sit amet
-                            massa vitae tortor. Nulla aliquet porttitor lacus luctus accumsan. Magna
-                            fermentum iaculis eu non diam phasellus vestibulum lorem. Purus non enim
-                            praesent elementum facilisis leo vel fringilla est.
-                        </Typography>
-                        <Typography
-                            sx={{
-                                marginTop: "15px",
-                                marginBottom: "15px",
-                            }}
-                        >
-                            Praesent semper feugiat nibh sed. Id consectetur purus ut faucibus.
-                            Neque viverra justo nec ultrices dui sapien. Pulvinar elementum integer
-                            enim neque volutpat. Quam elementum pulvinar etiam non quam lacus
-                            suspendisse faucibus interdum. Mi sit amet mauris commodo quis imperdiet
-                            massa. Et malesuada fames ac turpis. Ac ut consequat semper viverra nam
-                            libero justo laoreet. Dui id ornare arcu odio. Varius duis at
-                            consectetur lorem donec massa. Imperdiet dui accumsan sit amet nulla
-                            facilisi morbi tempus iaculis
-                        </Typography>
-                        <Typography>
-                            Gravida in fermentum et sollicitudin ac orci. Rhoncus est pellentesque
-                            elit ullamcorper dignissim. Amet consectetur adipiscing elit ut aliquam
-                            purus sit. Ipsum dolor sit amet consectetur adipiscing. Vulputate enim
-                            nulla aliquet porttitor lacus luctus. Risus nullam eget felis . |
-                        </Typography>
-                    </Box>
+                    />
                 </Styled.Story>
             </Styled.ViewStoryContainer>
         </>
