e4607d6369040b1f940d4990660778687e0b68c7
keith <keithohno@gmail.com>
2022-04-16 20:17:05 -0700

    made things look pretty

--- components/profile/results/index.tsx
+++ components/profile/results/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import {
     Typography,
     Divider,
@@ -9,7 +10,7 @@ import {
     CardMedia,
     CardContent,
     Pagination,
-    Box,
+    Stack,
     Tab,
     Tabs,
 } from "@mui/material";
@@ -18,26 +19,13 @@ import { useAuthContext } from "../../../context/authcontext";
 
 const ResultCard: React.FC<{ user2?: any }> = ({ user2 }) => {
     const { user } = useAuthContext();
-    const finalUser = user?.username! !== user2.username! ? user2 : user;
+    const finalUser = user?.username! === user2.username! ? user : user2;
 
     return (
-        <Card
-            sx={{
-                backgroundColor: "transparent",
-                textAlign: "center",
-                boxShadow: "none",
-                width: "17.5%",
-                borderRadius: "0",
-            }}
-        >
-            <CardMedia
-                component="img"
-                height="200px"
-                width="200px"
-                image=""
-                alt="Image"
-                style={{ backgroundColor: "grey" }}
-            />
+        <Styled.ResultCard>
+            <Styled.CardThumbnailContainer>
+                <Styled.CardThumbnail src="http://phototours.us/wp-content/uploads/2017/12/20036014.jpg" />
+            </Styled.CardThumbnailContainer>
             <CardContent>
                 <Typography variant="h5" component="div" fontWeight="bold">
                     Comic Title
@@ -49,7 +37,7 @@ const ResultCard: React.FC<{ user2?: any }> = ({ user2 }) => {
                     100 Views
                 </Typography>
             </CardContent>
-        </Card>
+        </Styled.ResultCard>
     );
 };
 
@@ -85,73 +73,53 @@ const ProfileCard: React.FC<{ user2?: any }> = ({ user2 }) => {
 
 const Hero: React.FC<{ user2?: any; userSubs?: any }> = ({ user2, userSubs }) => {
     const [tags, setTags] = useState<string[]>(["Comedy", "College"]);
-    const [category, setCategory] = useState<string>("Comics");
+    const [category, setCategory] = useState<string>("comics");
     const [time, setTime] = useState<string>("Today");
     const [sort, setSort] = useState<string>("alpha");
     const { user } = useAuthContext();
     const finalUser = user?.username! !== user2.username! ? user2 : user;
 
-    const handleChange = (_: any, val: any) => {
+    const onSetCategory = (_: any, val: any) => {
         setCategory(val);
+    };
+    const onSetSort = (e: any) => {
+        setSort(e.target.value);
+    };
+    const onSetTime = (e: any) => {
+        setTime(e.target.value);
     };
 
     return (
         <Styled.ResultsContainer>
-            <Styled.FilterContainer></Styled.FilterContainer>
-            <Tabs value={category} onChange={handleChange}>
+            <Tabs value={category} onChange={onSetCategory}>
                 <Tab label="Comics" value={"comics"} />
                 <Tab label="Stories" value={"stories"} />
                 <Tab label="Subscriptions" value={"subscriptions"} />
             </Tabs>
-            <Styled.TagListContainer>
-                <Typography variant="h4" sx={{ fontWeight: "bold", marginRight: "20px" }}>
-                    Tags:
-                </Typography>
-                <Styled.TagListContainer>
+            <Styled.TagSortFilterContainer>
+                <Styled.TagContainer>
+                    <Typography variant="h6" sx={{ fontWeight: "bold", marginRight: "20px" }}>
+                        Tags:
+                    </Typography>
                     {tags.map((val, index) => (
                         <Styled.Tag key={`${index}-tag`}>{val}</Styled.Tag>
                     ))}
-                </Styled.TagListContainer>
-                <Select
-                    labelId="sort-label"
-                    id="sort"
-                    value={sort}
-                    onChange={e => {
-                        setSort(e.target.value);
-                    }}
-                    label="Views"
-                    variant="standard"
-                    sx={{
-                        fontSize: "2rem",
-                        fontWeight: "bold",
-                    }}
-                >
-                    <MenuItem value={"alpha"}>A-Z</MenuItem>
-                    <MenuItem value={"views"}>Most Viewed</MenuItem>
-                    <MenuItem value={"rating"}>Highest Rated</MenuItem>
-                </Select>
-                <Select
-                    labelId="time-label"
-                    id="time"
-                    value={time}
-                    onChange={e => {
-                        setTime(e.target.value);
-                    }}
-                    label="Time"
-                    variant="standard"
-                    sx={{
-                        fontSize: "2rem",
-                        fontWeight: "bold",
-                        marginLeft: "20px",
-                    }}
-                >
-                    <MenuItem value={"Today"}>Today</MenuItem>
-                    <MenuItem value={"Week"}>This Week</MenuItem>
-                    <MenuItem value={"Month"}>This Month</MenuItem>
-                    <MenuItem value={"Year"}>This Year</MenuItem>
-                    <MenuItem value={"All"}>All Time</MenuItem>
-                </Select>
-            </Styled.TagListContainer>
+                </Styled.TagContainer>
+                <Styled.SortFilterContainer>
+                    <Select value={sort} onChange={onSetSort} label="Views" variant="standard">
+                        <MenuItem value={"alpha"}>A-Z</MenuItem>
+                        <MenuItem value={"views"}>Most Viewed</MenuItem>
+                        <MenuItem value={"rating"}>Highest Rated</MenuItem>
+                    </Select>
+                    <Select value={time} onChange={onSetTime} label="Time" variant="standard">
+                        <MenuItem value={"Today"}>Today</MenuItem>
+                        <MenuItem value={"Week"}>This Week</MenuItem>
+                        <MenuItem value={"Month"}>This Month</MenuItem>
+                        <MenuItem value={"Year"}>This Year</MenuItem>
+                        <MenuItem value={"All"}>All Time</MenuItem>
+                    </Select>
+                </Styled.SortFilterContainer>
+            </Styled.TagSortFilterContainer>
             <Divider sx={{ width: "100%", marginBottom: "20px" }} />
             {finalUser.username === user?.username && category === "Subscriptions" ? (
                 <Styled.CardsContainer>
@@ -160,19 +128,22 @@ const Hero: React.FC<{ user2?: any; userSubs?: any }> = ({ user2, userSubs }) =>
                     })}
                 </Styled.CardsContainer>
             ) : (
-                <Styled.CardsContainer>
+                <Stack>
                     <ResultCard user2={user2} />
                     <ResultCard user2={user2} />
                     <ResultCard user2={user2} />
                     <ResultCard user2={user2} />
-                </Styled.CardsContainer>
+                </Stack>
             )}
-            <Pagination />
-            <Typography variant="h6" component="div" sx={{ marginTop: "10px" }}>
-                {finalUser.username === user?.username && category === "Subscriptions"
-                    ? userSubs.length + "-" + userSubs.length + " Results"
-                    : "4-4 Results"}
-            </Typography>
+            <Styled.Pagination>
+                <Pagination />
+                {/* Personally not a fan of this: */}
+                {/* <Typography variant="h6" component="div" sx={{ marginTop: "10px" }}>
+                    {finalUser.username === user?.username && category === "Subscriptions"
+                        ? userSubs.length + "-" + userSubs.length + " Results"
+                        : "4-4 Results"}
+                </Typography> */}
+            </Styled.Pagination>
         </Styled.ResultsContainer>
     );
 };
--- components/profile/results/styles.ts
+++ components/profile/results/styles.ts
@@ -7,25 +7,31 @@ export const ResultsContainer = styled.div`
     flex-grow: 1;
 `;
 
-export const FilterContainer = styled.div`
+export const TagSortFilterContainer = styled.div`
     width: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: left;
-    margin-bottom: 10px;
+    margin: 10px;
 `;
 
-export const TagListContainer = styled.div`
+export const TagContainer = styled.div`
+    width: 100%;
     display: flex;
     flex-direction: row;
-    align-items: center;
+`;
+
+export const SortFilterContainer = styled.div`
+    align-self: right;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
 `;
 
 export const Tag = styled.div`
     background-color: white;
     border: 2px solid #39a78e;
     border-radius: 15px;
-    height: 30px;s
+    height: 30px;
     padding: 3px 10px;
     margin-right: 10px;
 `;
@@ -35,5 +41,27 @@ export const CardsContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
-    margin-bottom: 40px;
+`;
+
+export const ResultCard = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 100px;
+`;
+
+export const CardThumbnailContainer = styled.div`
+    width: 90px;
+    height: 90px;
+    overflow: hidden;
+    margin: 5px;
+`;
+
+export const CardThumbnail = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+
+export const Pagination = styled.div`
+    align-self: center;
 `;
