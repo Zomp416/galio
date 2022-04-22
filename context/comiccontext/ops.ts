5eb51c3734375717dee9c43918749c254771bc0e
clucidojr123 <clucidojr123@gmail.com>
2022-04-21 21:35:26 -0400

    add resize op

--- context/comiccontext/ops.ts
+++ context/comiccontext/ops.ts
@@ -15,11 +15,19 @@ interface DeleteLayerOpArgs {
 
 interface MoveLayerOpArgs {
     index: number;
-    dx: number;
-    dy: number;
+    x: number;
+    y: number;
 }
 
-export type OpArgs = AddLayerOpArgs | DeleteLayerOpArgs | MoveLayerOpArgs;
+interface ResizeLayerOpArgs {
+    index: number;
+    dw: number;
+    dh: number;
+    x: number;
+    y: number;
+}
+
+export type OpArgs = AddLayerOpArgs | DeleteLayerOpArgs | MoveLayerOpArgs | ResizeLayerOpArgs;
 
 // op for adding a new layer
 export const addLayerOp = (args: OpArgs, setLayers: any): Op => {
@@ -50,10 +58,38 @@ export const deleteLayerOp = (args: OpArgs, setLayers: any, layers: any): Op => 
 
 // op for deleteing a layer
 export const moveLayerOp = (args: OpArgs, setLayers: any, layers: any): Op => {
-    const { index, dx, dy } = args as MoveLayerOpArgs;
+    const { index, x, y } = args as MoveLayerOpArgs;
     const { ...removed } = layers[index];
-    removed.x += dx;
-    removed.y += dy;
+    removed.x = x;
+    removed.y = y;
+    return {
+        redo: () => {
+            setLayers((ls: ILayer[]) =>
+                ls
+                    .slice(0, index)
+                    .concat(removed)
+                    .concat(ls.slice(index + 1))
+            );
+        },
+        undo: () => {
+            setLayers((ls: ILayer[]) =>
+                ls
+                    .slice(0, index)
+                    .concat(layers[index])
+                    .concat(ls.slice(index + 1))
+            );
+        },
+    };
+};
+
+// op for resizing a layer
+export const resizeLayerOp = (args: OpArgs, setLayers: any, layers: any): Op => {
+    const { index, dw, dh, x, y } = args as ResizeLayerOpArgs;
+    const { ...removed } = layers[index];
+    removed.x = x;
+    removed.y = y;
+    removed.width += dw;
+    removed.height += dh;
     return {
         redo: () => {
             setLayers((ls: ILayer[]) =>
