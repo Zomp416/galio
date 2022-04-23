a9bdbe06390a4e77ca7a4bbf81ec1aac72f85512
keith <keithohno@gmail.com>
2022-04-23 12:16:28 -0700

    added debouncer for image search

--- components/comic/edit/widebar/properties/image.tsx
+++ components/comic/edit/widebar/properties/image.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { TextField, Typography, List, ListItem, Accordion, AccordionSummary } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import debounce from "lodash.debounce";
 
 const ImageProperties: React.FC = () => {
+    const doDebouncedSearch = debounce(query => {
+        console.log("triggered search ", query);
+    }, 800);
+
     return (
         <Accordion>
             <AccordionSummary expandIcon={<ExpandMoreIcon />} id="general-prop">
@@ -11,14 +16,11 @@ const ImageProperties: React.FC = () => {
             <List>
                 <ListItem>
                     <TextField
-                        id="image-name"
-                        name="imageName"
-                        label="Image Name"
+                        label="Search"
                         type="text"
                         variant="outlined"
-                        value={"Taquito"}
+                        onChange={e => doDebouncedSearch(e.target.value)}
                         fullWidth
-                        disabled
                     />
                 </ListItem>
             </List>
