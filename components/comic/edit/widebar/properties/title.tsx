a6253cd19210299fd798a17337d123807d3d2600
clucidojr123 <clucidojr123@gmail.com>
2022-04-23 18:02:17 -0400

    yes

--- components/comic/edit/widebar/properties/title.tsx
+++ components/comic/edit/widebar/properties/title.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { TextField, List, ListItem } from "@mui/material";
+import { useComicContext } from "../../../../../context/comiccontext";
 
 const TitleProperties: React.FC = () => {
+    const { comic, newdo } = useComicContext();
+
     return (
         <List>
             <ListItem>
-                <TextField name="name" label="Title" type="text" variant="outlined" fullWidth />
+                <TextField
+                    name="title"
+                    label="Title"
+                    type="text"
+                    variant="outlined"
+                    fullWidth
+                    value={comic ? comic.title : "Unnamed Comic"}
+                    onChange={e => {
+                        newdo("editComic", {
+                            squish: "title",
+                            title: e.target.value,
+                        });
+                    }}
+                />
             </ListItem>
             <ListItem>
                 <TextField
-                    name="name"
+                    name="description"
                     label="Description"
                     type="text"
                     variant="outlined"
@@ -17,6 +33,13 @@ const TitleProperties: React.FC = () => {
                     multiline
                     rows={10}
                     maxRows={10}
+                    value={comic ? comic.description : ""}
+                    onChange={e => {
+                        newdo("editComic", {
+                            squish: "description",
+                            description: e.target.value,
+                        });
+                    }}
                 />
             </ListItem>
         </List>
