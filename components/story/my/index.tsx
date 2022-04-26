d86d9a8e3d58c6673d62f765f2b49b6b8e388e22
Matt Ho <mattho@Matts-MacBook-Pro.local>
2022-04-26 00:00:33 -0400

    Fixed routing in My Stories

--- components/story/my/index.tsx
+++ components/story/my/index.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState } from "react";
 import { Typography } from "@mui/material";
-
+import { createStory } from "../../../util/zilean";
+import { useRouter } from "next/router";
 import * as Styled from "./styles";
 import EditTable from "./edittable";
 import PublishTable from "./publishtable";
@@ -21,10 +22,20 @@ const SelectionContext = createContext<ISelectionContext>({ selection: -1 });
 const MyStories: React.FC<StoryProps> = props => {
     const [filter, setFilter] = useState("edit");
     const [selection, setSelection] = useState(-1);
+    const router = useRouter();
 
     const onSetFilter = (e: any) => {
         if (filter !== e.target.value) setSelection(-1);
         setFilter(e.target.value);
+    };
+
+    //Used to create stories
+    const handleCreateStory = async () => {
+        //event.preventDefault(); having event prevents it from being a input in onclick
+        const data = await createStory();
+        if (!data.error) {
+            router.push({ pathname: "/story/edit/" + data.data._id });
+        }
     };
 
     return (
@@ -42,7 +53,7 @@ const MyStories: React.FC<StoryProps> = props => {
                             <Styled.ToggleButton value="edit">Editing</Styled.ToggleButton>
                             <Styled.ToggleButton value="publish">Published</Styled.ToggleButton>
                         </Styled.ToggleButtonGroup>
-                        <Styled.EditButton href="/story/create-new-story">
+                        <Styled.EditButton onClick={handleCreateStory}>
                             <Typography>New Story</Typography>
                             <AddIcon />
                         </Styled.EditButton>
